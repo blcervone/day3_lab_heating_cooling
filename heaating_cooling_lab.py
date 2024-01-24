5e0660ef1c181407685ab9f9fe95d4cf5a093a41
Brendan <brendan.cervone@gmail.com>
2024-01-24 13:37:56 -0500

    Second commit

--- heaating_cooling_lab.py
+++ heaating_cooling_lab.py
@@ -1,3 +1,4 @@
+# Define first function using parameters in if/else statements
 def heating_cooling(actual_temp, desired_temp):
     if actual_temp == desired_temp:
         print("Standby")
@@ -6,6 +7,7 @@ def heating_cooling(actual_temp, desired_temp):
     elif actual_temp < desired_temp:
         print("Run heat")
 
+# Define second function using parameters in if/else statements
 def convert_temp(temp_celsius, target_unit):
     if target_unit.upper() == 'C':
         print(f"{temp_celsius} degrees Celsius")
@@ -16,16 +18,27 @@ def convert_temp(temp_celsius, target_unit):
         temp = (temp_celsius * (9/5)) + 32
         print(f"{temp} degrees Fahrenheit")
 
+# Initialize flag variable for while loop signal
 keep_going = 'y'
 
+# Start while loop to ask user for function parameter input and output defined functions with input parameters
 while keep_going == 'y':
+    # Ask for values from user
     actual_temp = int(input("What is the current temperature? (in degrees Celsius) "))
+    print()
     desired_temp = int(input("What is your desired temperature? (in degrees Celsius) "))
+    print()
     temp_celsius = actual_temp
     target_unit = input("What unit would you like to convert to? (C, F, or K) ")
+    print()
 
+    # Instantiate defined functions with input parameters given by user
     heating_cooling(actual_temp,desired_temp)
+    print()
 
     convert_temp(temp_celsius, target_unit)
+    print()
 
+    # Ask if user would like to continue
     keep_going = input("Continue? (y/n) ")
+    print()
